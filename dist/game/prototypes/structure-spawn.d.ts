197d0aea8ab82e8f9571bf088647a575836c94f4
Austin Jones <implAustin@gmail.com>
2024-01-23 20:56:45 -0500

    add StructureSpawn.spawning

--- dist/game/prototypes/structure-spawn.d.ts
+++ dist/game/prototypes/structure-spawn.d.ts
@@ -8,12 +8,30 @@ declare module "game/prototypes" {
   } from "game/constants";
   import { Store } from "game/prototypes";
   export type STRUCTURE_SPAWN = "spawn";
+  export interface Spawning {
+    /**
+     * Time needed in total to complete the spawning.
+     */
+    needTime: number;
+    /**
+     * Remaining time to go.
+     */
+    remainingTime: number;
+    /**
+     *
+     */
+    creep: Creep;
+  }
   // export const STRUCTURE_SPAWN: STRUCTURE_SPAWN;
   export interface StructureSpawn extends OwnedStructure<STRUCTURE_SPAWN> {
     /**
      * A Store object that contains a cargo of this structure. Spawns can contain only energy.
      */
     store: Store<ResourceConstant>;
+    /**
+     * If the spawn is in process of spawning a new creep, this object will contain a Spawning object, or null otherwise.
+     */
+    spawning: Spawning | null;
     /**
      * Start the creep spawning process. The required energy amount can be withdrawn from all spawns and extensions in the room.
      * @returns A creep on success or an errorcode on failure
@@ -25,7 +43,7 @@ declare module "game/prototypes" {
   }
   interface StructureSpawnConstructor
     extends _Constructor<StructureSpawn>,
-      _ConstructorById<StructureSpawn> {}
+    _ConstructorById<StructureSpawn> { }
 
   export const StructureSpawn: StructureSpawnConstructor;
 }
